886b738602604ec36670516dcdef9c66882d2e2a
berihunchalachew <40446808+berihunchalachew@users.noreply.github.com>
2026-04-05 16:59:17 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME="student-app-v4";
+const FILES_TO_CACHE=[
+  "./","./index.html","./manifest.json",
+  "./icons/icon-192.png","./icons/icon-512.png",
+  "./icons/login.png","./icons/diredawa-university.png"
+];
+
+// Install
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)));
+});
+
+// Activate
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>{ if(key!==CACHE_NAME) return caches.delete(key);}))));
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch",e=>{
+  if(e.request.method==="POST" && e.request.url.includes("script.google.com")){
+    e.respondWith(fetch(e.request).catch(()=>new Response(JSON.stringify({error:"offline"}),{headers:{"Content-Type":"application/json"}})));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request).then(net=>{
+    return caches.open(CACHE_NAME).then(cache=>{cache.put(e.request,net.clone()); return net;});
+  })).catch(()=>caches.match("./index.html")));
+});
